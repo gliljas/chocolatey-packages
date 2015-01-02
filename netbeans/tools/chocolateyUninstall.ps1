7e714fccbda04d3a25f06cf0473c1471af9dfd1b
ComFreek <comfreek@outlook.com>
2015-01-02 16:44:28 +0100

    Uninstaller now reads the path from registry

--- netbeans/tools/chocolateyUninstall.ps1
+++ netbeans/tools/chocolateyUninstall.ps1
@@ -1,4 +1,26 @@
-$uninstallExe = Join-Path $env:ProgramFiles "NetBeans 8.0.2\uninstall.exe"
-$uninstallArgs = '--silent'
-$validExitCodes = @(0)
-Start-ChocolateyProcessAsAdmin $uninstallArgs $uninstallExe -validExitCodes $validExitCodes
+function Get-UninstallString {
+	$regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-nb-base-8.0.2.0.201411181905'
+	$key = Get-Item -Path $regPath -ErrorAction Stop
+	$uninstallString = $key.GetValue('UninstallString')
+	
+	if ($uninstallString) {
+		return $uninstallString
+	}
+	else {
+		throw [System.IO.FileNotFoundException] "Uninstall string not found in `"$regPath`"."
+	}
+}
+
+$packageName = 'NetBeans Java SE'
+
+try {
+	$uninstallArgs = '--silent'
+	$validExitCodes = @(0)
+	Start-ChocolateyProcessAsAdmin $uninstallArgs $(Get-UninstallString) -validExitCodes $validExitCodes
+	
+	Write-ChocolateySuccess $packageName
+}
+catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
