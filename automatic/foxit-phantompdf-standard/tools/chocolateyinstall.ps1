1e5556d58a82533380692b78b71af626263c1ed2
ComFreek <comfreek@outlook.com>
2015-08-01 17:34:17 +0200

    Foxit PhantomPDF standard
    Without multilanguage support

---
+++ automatic/foxit-phantompdf-standard/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'foxit-phantompdf-standard'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url = '{{DownloadUrl}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+
+  # Log file specification has been removed: " /l*v '$env:TEMP\chocolatey\$packageName\install.log'"
+  # It always failed within my virtual machine with the error:
+  # Error 1622, error opening installation log file. Verify that the specified log file
+  # location exists and is writable.
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+
+  # optional
+  checksum      = '{{Checksum}}'
+  checksumType  = 'md5'
+  checksum64    = '{{Checksumx64}}'
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/foxit-phantompdf-standard/tools/chocolateyuninstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'foxit-phantompdf-standard'
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+Try {
+  # We cannot rely on the MSI GUID as it sometimes changed in our tests
+  $msiProductCodeGuid = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+      | ?{ Test-Path $_ } `
+      | Get-ChildItem `
+      | % {
+        Try {
+          $_ | Get-ItemProperty
+        }
+        Catch [System.InvalidCastException] {
+          # This is an internal PowerShell v3 bug where Get-ItemProperty fails
+          # when reading a REG_DWORD value which contains binary data.
+          # See http://connect.microsoft.com/PowerShell/feedbackdetail/view/801007/get-itemproperty-specified-cast-is-not-valid-error-when-reg-dword-value-has-binary-data
+          return @{}
+        }
+      } `
+      | Where-Object -Property DisplayName -eq -Value "Foxit PhantomPDF Standard" `
+      | Select-Object -ExpandProperty PSChildName
+}
+Catch [System.InvalidCastException] {
+}
+if ($msiProductCodeGuid -eq $null) {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'MSI' 
+$silentArgs = "$msiProductCodeGuid /qn /norestart"
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+
+if ($shouldUninstall) {
+  Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File ''
+}
