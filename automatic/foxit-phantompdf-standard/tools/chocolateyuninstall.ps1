6b0cca504726cff4e88c98c8e961f7471def0adf
ComFreek <comfreek@outlook.com>
2015-08-01 17:41:17 +0200

    Remove redundant try-catch

--- automatic/foxit-phantompdf-standard/tools/chocolateyuninstall.ps1
+++ automatic/foxit-phantompdf-standard/tools/chocolateyuninstall.ps1
@@ -8,27 +8,24 @@ $local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\
 $machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 $machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 
-Try {
-  # We cannot rely on the MSI GUID as it sometimes changed in our tests
-  $msiProductCodeGuid = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
-      | ?{ Test-Path $_ } `
-      | Get-ChildItem `
-      | % {
-        Try {
-          $_ | Get-ItemProperty
-        }
-        Catch [System.InvalidCastException] {
-          # This is an internal PowerShell v3 bug where Get-ItemProperty fails
-          # when reading a REG_DWORD value which contains binary data.
-          # See http://connect.microsoft.com/PowerShell/feedbackdetail/view/801007/get-itemproperty-specified-cast-is-not-valid-error-when-reg-dword-value-has-binary-data
-          return @{}
-        }
-      } `
-      | Where-Object -Property DisplayName -eq -Value "Foxit PhantomPDF Standard" `
-      | Select-Object -ExpandProperty PSChildName
-}
-Catch [System.InvalidCastException] {
-}
+# We cannot rely on the MSI GUID as it sometimes changed in our tests
+$msiProductCodeGuid = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+  | ?{ Test-Path $_ } `
+  | Get-ChildItem `
+  | % {
+    Try {
+      $_ | Get-ItemProperty
+    }
+    Catch [System.InvalidCastException] {
+      # This is an internal PowerShell v3 bug where Get-ItemProperty fails
+      # when reading a REG_DWORD value which contains binary data.
+      # See http://connect.microsoft.com/PowerShell/feedbackdetail/view/801007/get-itemproperty-specified-cast-is-not-valid-error-when-reg-dword-value-has-binary-data
+      return @{}
+    }
+  } `
+  | Where-Object -Property DisplayName -eq -Value "Foxit PhantomPDF Standard" `
+  | Select-Object -ExpandProperty PSChildName
+
 if ($msiProductCodeGuid -eq $null) {
     Write-Host "$packageName has already been uninstalled by other means."
     $shouldUninstall = $false
