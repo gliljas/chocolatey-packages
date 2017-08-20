a2cd0f737474f58c2c5764d588c2015fe9387dfa
ComFreek <comfreek@outlook.com>
2017-08-20 16:06:14 +0200

    [FoxitReader] Update
    Has been pushed to Chocolatey long ago, though.

--- foxitreader/tools/chocolateyInstall.ps1
+++ foxitreader/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32       = 'https://www.foxitsoftware.com/downloads/latest.php?product=Foxit-Reader&platform=Windows&version=8.3.0.14878&package_type=exe&language=English'
-$checksum32  = 'f1a4c530740cfcb5afc4ee6f335b4b69137a21809c85128a19a609a8f09c0ca6'
+$url32       = 'https://www.foxitsoftware.com/downloads/latest.php?product=Foxit-Reader&platform=Windows&version=8.3.1.21155&package_type=exe&language=English'
+$checksum32  = '1207fac5f7dd69689be65b6289b08a3775507420044a359c755a1b52f4841ad2'
 
 $packageArgs = @{
 	packageName    = 'foxitreader'
